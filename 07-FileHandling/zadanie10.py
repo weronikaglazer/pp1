55dbca92326a4a5e784f4c855a7c53077f548849
s-013-25 <s-013-25@gumiforest.ae.krakow.pl>
2022-12-10 13:59:19 +0100

    ćwiczenia 10.12

---
+++ 07-FileHandling/zadanie10.py
@@ -0,0 +1,8 @@
+file = open('personal.txt', 'w')
+
+file.write("Weronika\n")
+file.write("Glazer\n")
+file.write("Krakow University of Economics\n")
+file.write("Computer Science\n")
+
+file.close()
