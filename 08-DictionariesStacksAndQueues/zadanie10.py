36a6701d0c5599ea0cb854644ca861f354386f49
s-013-25 <s-013-25@gumiforest.ae.krakow.pl>
2022-12-10 14:37:51 +0100

    ćwiczenia 10.12

---
+++ 08-DictionariesStacksAndQueues/zadanie10.py
@@ -0,0 +1,11 @@
+countries = [
+    {"name": "Poland", "population": 37000000},
+    {"name": "Sweden", "population": 10000000},
+    {"name": "USA", "population": 331000000},
+    {"name": "Finland", "population": 5000000},
+    {"name": "Ireland", "population": 5000000}
+]
+i = 0
+while i < len(countries):
+    print(countries[i])
+    i += 1
---
+++ 08-DictionariesStacksAndQueues/zadanie9.py
@@ -0,0 +1,11 @@
+phone = {
+    "name": "iPhone",
+    "model": 12,
+    "brand": "Apple",
+    "color": "black",
+    "works": True,
+    "features": ["camera", "face ID", "games", "wallet"]
+}
+
+for k, v in phone.items():
+    print(k, ":", v)
