6698d6e4dfcde1c8fe9b0c22776f463cba8b6931
Weronika Glazer <weronikaglazer@gmail.com>
2023-01-13 12:26:15 +0100

    updated p5

--- 09-Test2/p5.py
+++ 09-Test2/p5.py
@@ -2,16 +2,15 @@ import re
 
 def f(first_letter,last_letter):
     with open("data.txt", "r") as file:
-        lines = file.readlines()
         counter = 0
-        for line in lines:
-            matches = re.findall(f"^{first_letter}\w*{last_letter}$", line) 
+        for line in file:
+            line = line.rstrip()
+            matches = re.findall(f"{first_letter}\w+{last_letter}", line) 
             if len(matches) != 0:
                 counter += len(matches)
             else:
                 continue
             
-        
         return counter
 
 
